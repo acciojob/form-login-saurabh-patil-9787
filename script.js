67385a9d2b8073a9d107906ca5ab9b2abafc10dc
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-01-07 19:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+let inputes = document.querySelectorAll('input')
 
+
+
+function getFormvalue(){
+    let firsName = inputes[0].value;
+let secoundName= inputes[1].value;
+
+    alert(`${firsName} ${secoundName}`);
 }
